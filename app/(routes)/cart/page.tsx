068d2ed030807c8d8f1fc72dd2338ea35d7a6f6a
gflacruz <glacruz@zoho.com>
2023-09-15 09:06:20 -0400

    changed cart and added cartinfo

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,20 +1,22 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import Container from "@/components/ui/container";
-import useCart from "@/hooks/use-cart";
-import CartItem from "./components/cart-item";
-import Summary from "./components/summary";
+import Container from '@/components/ui/container';
+import useCart from '@/hooks/use-cart';
+
+import Summary from './components/summary'
+import CartItem from './components/cart-item';
+
+export const revalidate = 0;
 
 const CartPage = () => {
   const [isMounted, setIsMounted] = useState(false);
+  const cart = useCart();
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
-
-  const cart = useCart();
 
   if (!isMounted) {
     return null;
@@ -27,9 +29,7 @@ const CartPage = () => {
           <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
           <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
             <div className="lg:col-span-7">
-              {cart.items.length === 0 && (
-                <p className="text-neutral-500">No Items added to cart</p>
-              )}
+              {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart.</p>}
               <ul>
                 {cart.items.map((item) => (
                   <CartItem key={item.id} data={item} />
@@ -41,7 +41,7 @@ const CartPage = () => {
         </div>
       </Container>
     </div>
-  );
+  )
 };
 
 export default CartPage;
